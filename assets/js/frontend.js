1e75073edac30bbef73c637ea1ebc5ec39535029
Akramul Hasan <lighthouse8048@gmail.com>
2025-04-19 05:58:48 +0600

    make the messages howing js function compatilbe with Block theme

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -15,12 +15,38 @@ jQuery(function ($) {
           if (response.success) {
             // Update messages
             if (response.data.messages) {
-              if ($(".wc-cart-manager-messages").length) {
-                $(".wc-cart-manager-messages").replaceWith(
-                  response.data.messages
+              // Check if we're on a Cart Block page
+              const isCartBlock =
+                document.querySelector(".wp-block-woocommerce-cart") !== null;
+
+              if (isCartBlock) {
+                // Handle Cart Block
+                const cartBlock = document.querySelector(
+                  ".wp-block-woocommerce-cart"
                 );
+                let messageContainer = cartBlock.querySelector(
+                  ".wc-cart-manager-messages"
+                );
+
+                if (!messageContainer) {
+                  messageContainer = document.createElement("div");
+                  messageContainer.className = "wc-cart-manager-messages";
+                  cartBlock.insertBefore(
+                    messageContainer,
+                    cartBlock.firstChild
+                  );
+                }
+
+                messageContainer.innerHTML = response.data.messages;
               } else {
-                $(".woocommerce-cart-form").before(response.data.messages);
+                // Handle Classic Cart
+                if ($(".wc-cart-manager-messages").length) {
+                  $(".wc-cart-manager-messages").replaceWith(
+                    response.data.messages
+                  );
+                } else {
+                  $(".woocommerce-cart-form").before(response.data.messages);
+                }
               }
             } else {
               $(".wc-cart-manager-messages").remove();
@@ -46,45 +72,52 @@ jQuery(function ($) {
   );
 
   // Update on quantity changes
-  $(".woocommerce-cart-form").on("change", "input.qty", function () {
-    var $form = $(this).closest("form");
-    var $button = $form.find("button[name='update_cart']");
-    var $row = $(this).closest("tr");
-    var cart_item_key = $row.attr("data-cart_item_key");
-    var quantity = $(this).val();
+  $(".woocommerce-cart-form, .wp-block-woocommerce-cart").on(
+    "change",
+    "input.qty",
+    function () {
+      var $form = $(this).closest("form");
+      var $button = $form.find("button[name='update_cart']");
+      var $row = $(this).closest("tr, .wc-block-cart-item");
+      var cart_item_key = $row.attr("data-cart_item_key");
+      var quantity = $(this).val();
 
-    // Disable the update button
-    $button.prop("disabled", true);
+      // Disable the update button
+      $button.prop("disabled", true);
 
-    // Send AJAX request to update quantity
-    $.ajax({
-      url: wc_cart_params.ajax_url,
-      type: "POST",
-      data: {
-        action: "woocommerce_update_cart",
-        cart_item_key: cart_item_key,
-        quantity: quantity,
-        security: wc_cart_params.update_cart_nonce,
-      },
-      success: function (response) {
-        if (response.success) {
-          // Update cart fragments
-          $(document.body).trigger("wc_fragment_refresh");
-          // Update cart messages
-          updateCartMessages();
-        }
-      },
-      complete: function () {
-        // Re-enable the update button
-        $button.prop("disabled", false);
-      },
-    });
-  });
+      // Send AJAX request to update quantity
+      $.ajax({
+        url: wc_cart_params.ajax_url,
+        type: "POST",
+        data: {
+          action: "woocommerce_update_cart",
+          cart_item_key: cart_item_key,
+          quantity: quantity,
+          security: wc_cart_params.update_cart_nonce,
+        },
+        success: function (response) {
+          if (response.success) {
+            // Update cart fragments
+            $(document.body).trigger("wc_fragment_refresh");
+            // Update cart messages
+            updateCartMessages();
+          }
+        },
+        complete: function () {
+          // Re-enable the update button
+          $button.prop("disabled", false);
+        },
+      });
+    }
+  );
 
   // Update after form submission
-  $(".woocommerce-cart-form").on("submit", function () {
-    setTimeout(updateCartMessages, 1000);
-  });
+  $(".woocommerce-cart-form, .wp-block-woocommerce-cart").on(
+    "submit",
+    function () {
+      setTimeout(updateCartMessages, 1000);
+    }
+  );
 
   // Additional handler for cart fragments refresh
   $(document.body).on("wc_fragments_refreshed", function () {
